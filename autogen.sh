0962b5840c6d5225e8237ac6cc0ff0466ed8b18a
Riduan <riduan@riduan.de>
2015-12-25 23:49:38 +0100

    update autogen

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
-libtoolize
+
+case `uname` in 
+    
+    Darwin*) 
+        glibtoolize ;;
+    
+    *) 
+    libtoolize;; 
+esac
+
+export PKG_CONFIG_PATH="/opt/local/lib/pkgconfig:/usr/local/lib/pkgconfig"
+
 aclocal
 autoconf
 autoheader
 automake -a -c
 autoreconf -i
 ./configure --disable-dependency-tracking
+
+
